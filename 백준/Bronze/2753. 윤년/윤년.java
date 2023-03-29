9142be7035560be18632fe7dad4aaaf988e639f2
Sungnam <126052997+VIichan16@users.noreply.github.com>
2023-03-29 11:13:49 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+
+
+
+
+        if(num1 % 4 ==0) {
+            if(num1 % 400 ==0){
+                System.out.println("1");
+
+            } else if (num1% 100 ==0) {
+
+                System.out.println("0");
+            }
+            else {
+                System.out.println("1");
+            }
+        }else {
+            System.out.println("0");
+        }
+
+
+    }
+}
